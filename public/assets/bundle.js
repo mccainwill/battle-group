b72b4271c86daab690d2548df3373a5505bea3b7
mccainwill <mccainwill@gmail.com>
2016-05-11 11:39:48 -0400

    bundle.js updates

--- public/assets/bundle.js
+++ public/assets/bundle.js
@@ -14524,7 +14524,7 @@ var SingleHeroModel = Backbone.Model.extend({
     initialize: function initialize() {
         this.url = function () {
 
-            var marvelKey = 'apikey=cd80e84f4acc3f0d2cdabd391244ab24';
+            var marvelKey = 'apikey=fedd59d6507d8fd892bb0a51b80b65ab';
 
             //var marvelKey = 'apikey=41f7d8754906d29d9b0dd03e19b6138a';
 
